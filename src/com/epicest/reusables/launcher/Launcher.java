880f37fd93be04b4d275e32c43d6e765b383d62e
Michael <mjsprogrammer@gmail.com>
2016-11-08 07:39:27 -0700

    Last Minute Stuff

--- src/com/epicest/reusables/launcher/Launcher.java
+++ src/com/epicest/reusables/launcher/Launcher.java
@@ -1,13 +1,29 @@
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package com.epicest.reusables.launcher;
 
+import javax.swing.JFrame;
+
 /**
  * A gui Launcher, designed to be like Bethesda's game launcher
  *
  * @author EpicestGamer
  */
-public class Launcher {
+public class Launcher extends JFrame {
+
+    public Launcher() {
+        super("Pixelcraft Game Launcher");
+        setUndecorated(true);
+        setSize(800, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    protected void drawButtons() {
+    }
 }
